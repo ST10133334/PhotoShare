219133b6587118914a619cfbe96430ec7fe10c7b
Saien <ST10133334@vcconnect.edu.za>
2025-08-11 13:53:34 +0200

    Database connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,49 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
+import fs from "fs";
+import https from "https";
+import mongoose from "mongoose";
 
-dotenv.config()
 
-const app = express()
-const PORT = process.env.PORT || 5000;
+//Importing enviroment variables
+dotenv.config();
 
+//Initialize the express application
+const app = express();
+const PORT = process.env.PORT || 8000;
+//const MONGODB_URI = process.env.MONGO_URI
+const MONGO_URI="mongodb://localhost:27017/PhotoShare"
+
+
+//Add middleware to parse JSON requests
 app.use(express.json());
 
+//Test route
 app.get("/", (req, res) => {
     res.send("Welcome to the PhotoShare API");
 });
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log("Server is Running on port + ${process.env.PORT || 8000}");
-})
+//Load SSL certificates
+const sslOptions = {
+    key:fs.readFileSync('certs/localhost-key.pem'),
+    cert: fs.readFileSync('certs/localhost.pem')
+}
+
+//Add Database connection
+mongoose.connect(MONGO_URI, {
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+});
+    
+
+//Create HTTPS server
+https.createServer(sslOptions, app).listen(PORT, () => {
+    console.log(`HTTPS Server is running on port ${PORT}`);
+});
+
+//Start the server
+//app.listen(process.env.PORT || 5000, () => { 
+//  console.log("Server is Running on port + ${process.env.PORT || 8000}");
+//});
